a8db0843eb90964dbcf077682c248b73f42baf2c
Aaron Minyard <amnyrrd@gmail.com>
2019-12-20 23:01:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,5 @@
 def find_element_index(array, value_to_find)
-  # array.index(value_to_find) 
-  i = 0
-  while i < array.length 
-  
+  array.index(value_to_find) 
 end
 
 def find_max_value(array)
